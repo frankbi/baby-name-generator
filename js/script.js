c9bed6ceabd7afb28d30caf768e83320ebd9cf01
Frank Bi <fbi@newshour.org>
2014-05-29 13:26:12 -0400

    google spreadsheet hookup

--- js/script.js
+++ js/script.js
@@ -39,7 +39,7 @@ var generator = {
 		var source = $("#result").html();
 		var template = Handlebars.compile(source);
 
-		generator.geName = name.firstName + suffix;
+		generator.genName = name.firstName + suffix;
 
 		data = {
 			"fullName": name.fullName,
@@ -52,13 +52,26 @@ var generator = {
 
 	submitPoll: function(status) {
 
+		$(".likes").html(function() {
+			var text1 = "<h2>Loading...</h2>";
+			var image = "<img class='loading' src='img/loading.gif'>";
+			return text1 + image;
+		});
+
 		$.ajax({
 			type: "POST",
 			url: "store.php",
 			data: {
 				"genName": generator.genName,
-				"fullName": generator.name,
+				"fullName": generator.name.fullName,
 				"status": status 
+			},
+			success: function() {
+				$(".likes").html(function() {
+					var text1 = "<h2>Your Response Has Been Recorded!</h2>";
+					var text2 = "<a target='_blank' href='http://bit.ly/1kkWPt9'><h2>View Responses</h2></a>"
+					return text1 + text2;
+				});
 			}
 		})
 
--- store.php
+++ store.php
@@ -1,6 +1,29 @@
 <?php
 
+	$GENERATED_NAME = $_POST['genName'];
+	$FULL_NAME = $_POST['fullName'];
+	$STATUS = $_POST['status'];
 
+	if ($STATUS == 0) {
+		$STATUS = "";
+	} else if ($STATUS == 1) {
+		$STATUS = "TRUE";
+	}
+
+	include "creds.php";
+	include_once("Google_Spreadsheet.php");
+
+	$ss = new Google_Spreadsheet($user, $pass);
+	$ss->useWorksheet("Tallies");
+	$ss->useSpreadsheet("baby-name-generator");
+
+	$row = array (
+		"Generated Name" => $GENERATED_NAME,
+		"Derived From" => $FULL_NAME,
+		"Like" => $STATUS
+	);
+
+	$ss->addRow($row);
 
 
 ?>
